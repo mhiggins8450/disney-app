8b2c29cd388caccfb1636527d45c59dc4a6924f4
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-30 21:58:19 -0500

    Making further edits to Header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Header.css';
-import shopDisneyLogo from '../Images/logo.png';
+import shopDisneyLogo from '../Images/logo.png'; // Import the logo file
 
 function Header() {
   return (
     <header className="App-header">
       <div className="left-icons">
         <div className="hamburger-menu">&#9776;</div>
-        <img
-          src={shopDisneyLogo}
-          alt="shopDisney Logo"
-          className="logo"
-        />
+        <div className="logo-container">
+          <img
+            src={shopDisneyLogo}
+            alt="shopDisney Logo"
+            className="logo"
+          />
+        </div>
       </div>
       <div className="right-icons">
         <span className="blue-heart">&#9829;</span>

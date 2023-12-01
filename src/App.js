44c1c34e86f4a3a684ea2f14b7335eae6a1f0170
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-30 21:15:12 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import Banner from './components/Banner/banner';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Shop</h1>
-      </header>
-      <main>
-        <p>This is where your main content will go.</p>
-      </main>
-      <footer>
-        <p>&copy; 2023 My Shop. All rights reserved.</p>
-      </footer>
       <Banner />
     </div>
   );

bd687507906703ad063dc2843f5b4058be28e0e0
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-30 21:54:48 -0500

    Added header section; making adjustments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Banner from './components/Banner/banner';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import shopDisneyLogo from '../Images/logo.png';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <div className="left-icons">
+        <div className="hamburger-menu">&#9776;</div>
+        <img
+          src={shopDisneyLogo}
+          alt="shopDisney Logo"
+          className="logo"
+        />
+      </div>
+      <div className="right-icons">
+        <span className="blue-heart">&#9829;</span>
+        <span className="shopping-bag">&#128717;</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
